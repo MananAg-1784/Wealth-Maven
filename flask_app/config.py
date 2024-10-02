317bd3c1e778173fb0d9c6e50aa1ab5d513b4078
Itachi-o-o <mananagarwal1784@gmail.com>
2024-10-02 23:32:16 +0530

    Update config.py

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,13 +1,15 @@
 email_creds = {
-    "email" : "random",
-    "password": "random"
+    "email" : "",
+    "password": ""
 }
 
 database_endpoint = {
     "host" :"127.0.0.1",
     "port" :3306,
-    "password":"1234",
-    "user":"root",
-    "database":"wealthmaven",
+    "password":"wealth_maven@2024",
+    "user":"wealth_maven",
+    "database":"wealth_maven",
     "autocommit":True
 }
+
+website_url = "127.0.0.1:8080"
